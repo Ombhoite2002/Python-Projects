4226f83440087c7a3c6ccf9e4a6c489945fcaf14
Ombhoite2002 <om.bhoite2002@gmail.com>
2024-10-06 18:56:04 +0530

    Quiz game project

---
+++ quiz.py
@@ -0,0 +1,28 @@
+import ast
+
+name = input("Enter your name: ")
+print(f"Hello {name}, welcome to this quiz game.")
+
+
+with open('questions.txt','r') as f:
+    content = f.read()
+
+
+questions_dict = ast.literal_eval(content)
+
+questions = list(questions_dict.keys())
+score = 0
+
+for question in questions:
+    print(question)
+    my_answer = input("Enter your answer: ").lower()
+    
+    
+    if my_answer == questions_dict[question].lower():
+        print("Correct!")
+        score += 1
+    else:
+        print(f"Incorrect! The correct answer is: {questions_dict[question]}")
+
+
+print(f"You got {score} answers correct out of {len(questions)}.")
